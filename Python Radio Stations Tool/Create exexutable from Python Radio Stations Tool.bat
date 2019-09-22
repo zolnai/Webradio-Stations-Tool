c99c961fd5f24a4da831e765b5f36ba12ffbcb2a
zolnai <55508161+zolnai@users.noreply.github.com>
2019-09-22 14:13:39 +0200

    Delete Create exexutable from Python Radio Stations Tool.bat

--- Python Radio Stations Tool/Create exexutable from Python Radio Stations Tool.bat
+++
@@ -1,5 +0,0 @@
-rem Install PyInstaller from PyPI:
-pip install pyinstaller
-rem Go to your program’s directory and run:
-pyinstaller "Python Radio Stations Tool.py"
-rem This will generate the bundle in a subdirectory called dist.
